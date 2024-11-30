603f3ec9efd0faf596ae07ae42ecc109acf0e237
andreesantt <andresanttdev@gmail.com>
2024-11-30 10:49:58 -0300

    Update 4-add_chart.py

--- 4-add_chart.py
+++ 4-add_chart.py
@@ -13,17 +13,16 @@ max_row = wb.active.max_row
 # 3 - Adcionando Dados e Categorias no Gráfico
 bar_chart = BarChart()
 
-data = Reference(
+categories = Reference(
     sheet,
     min_col=min_column + 1,
-    max_col=max_column,
+    max_col=min_column,
     min_row=min_row,
     max_row=max_row
 )
 
 bar_chart.add_data(data, titles_from_data=True)
-bar_chart.set_categories()
-
+bar_chart.set_categories(categories)
 # 4 - Criango grágico
 sheet.add_chart(bar_chart, 'B10')
 bar_chart.title = 'Vendas por Fabricantes'
